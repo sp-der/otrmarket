34737fd82cb6ef6525cc93da95bdd2f0ce6c1253
sp-der <98449997+sp-der@users.noreply.github.com>
2026-08-15 15:47:53 -0700

    type rejection block setup trigger

--- src/strategies/models.py
+++ src/strategies/models.py
@@ -115,7 +115,7 @@ class StrategySetup:
     direction: Direction
     created_at: datetime
     pd_array: FairValueGap
-    trigger_type: Literal["liquidity_sweep", "smt"]
+    trigger_type: Literal["liquidity_sweep", "smt", "rejection_block"]
     trigger_details: dict[str, Any]
     displacement: Displacement
     entry_fvg: FairValueGap
